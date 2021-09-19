dfca55f50c3279b57a36fef4887a62949620a4e6
sydneyw-cyber <sydneyw521@gmail.com>
2021-09-18 22:35:50 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express')
 const path = require('path');
+
+const { typeDefs, resolvers } = require("./schemas")
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+
+async function startServer() {
+  await server.start()
+  server.applyMiddleware({ app })
+}
+
+startServer()
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
