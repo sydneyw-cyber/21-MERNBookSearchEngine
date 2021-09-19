457b9aab17e274b7c65752532ac145b67026b05c
sydneyw-cyber <sydneyw521@gmail.com>
2021-09-18 22:53:17 -0500

    utils

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+          _id
+          username
+          savedBooks {
+            bookId
+          }
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+                _id
+                username
+                savedBooks {
+                    _id
+                    authors
+                    description
+                }
+        }
+    }
+`
